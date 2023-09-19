b6a45d40eee7db3ba54dfd0beb44dbd502c74248
Raphael Wagner <31208448+raalwa@users.noreply.github.com>
2023-09-19 22:36:59 +0200

    Change API parameters to premium API

--- src/config_EXAMPLE.py
+++ src/config_EXAMPLE.py
@@ -1,15 +1,13 @@
-from datetime import datetime, timezone
-
 LOGS_PATH = 'logs'
 LOG_FILENAME = 'weather_log.csv'
-FIELDNAMES = ['validdate', 't_2m:C', 'uv:idx']
+FIELDNAMES = ['validdate', 'direct_rad:W', 'diffuse_rad:W', 'clear_sky_rad:W',
+              'global_rad:W', 'uv:idx', 'is_day:idx', 'sun_azimuth:d', 'sun_elevation:d', 'current', 'voltage', 'power']
 STARTTIME = '00:02:50'
 GOOGLE_DRIVE_FOLDER_ID = ''
-INTERVAL_MEASUREMENTS = 20
-INTERVAL_BACKUP = 25
+INTERVAL_MEASUREMENTS = 2
+INTERVAL_BACKUP = 2
 # Parameters for API call
-VALIDDATE = f'{datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")}'
-PARAMETERS = 't_2m:C,uv:idx'
+PARAMETERS = 'direct_rad:W,diffuse_rad:W,clear_sky_rad:W,global_rad:W,uv:idx,is_day:idx,sun_azimuth:d,sun_elevation:d'
 LOCATION = ''
 FORMAT = 'json'
 TOKEN_URL = 'https://login.meteomatics.com/api/v1/token'
