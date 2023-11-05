0870f6dc72f93e59ebb4a8956b21e25acf50a98b
Raphael Wagner <31208448+raalwa@users.noreply.github.com>
2023-11-05 22:55:04 +0100

    Change API to openweathermap

--- src/config_EXAMPLE.py
+++ src/config_EXAMPLE.py
@@ -1,16 +1,14 @@
 LOGS_PATH = 'logs'
 LOG_FILENAME = 'weather_log.csv'
-FIELDNAMES = ['validdate', 'direct_rad:W', 'diffuse_rad:W', 'clear_sky_rad:W',
-              'global_rad:W', 'uv:idx', 'is_day:idx', 'sun_azimuth:d', 'sun_elevation:d', 'current', 'voltage', 'power']
+IELDNAMES = ['dt', 'visibility', 'clouds', 'uvi', 'temp', 'wind_speed', 'wind_deg',
+             'wind_gust', 'sunrise', 'feels_like', 'sunset', 'pressure', 'dew_point', 'humidity', 'current', 'voltage', 'power']
 STARTTIME = '00:02:50'
 GOOGLE_DRIVE_FOLDER_ID = ''
-INTERVAL_MEASUREMENTS = 2
-INTERVAL_BACKUP = 2
+INTERVAL_MEASUREMENTS_MIN = 2
+INTERVAL_BACKUP_H = 2
 # Parameters for API call
-PARAMETERS = 'direct_rad:W,diffuse_rad:W,clear_sky_rad:W,global_rad:W,uv:idx,is_day:idx,sun_azimuth:d,sun_elevation:d'
-LOCATION = ''
-FORMAT = 'json'
-TOKEN_URL = 'https://login.meteomatics.com/api/v1/token'
-API_URL = 'https://api.meteomatics.com'
-USER = ''
-PASS = ''
+LATITUDE = '48.77974618079882'
+LONGITUDE = '12.247728587655049'
+API_URL = 'https://api.openweathermap.org/data/3.0/onecall?'
+API_KEY = '3a687bec467f086a8bf07e990402c9b2'
+EXCLUDE = 'minutely,hourly,daily,alerts'
