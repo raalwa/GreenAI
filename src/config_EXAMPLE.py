c6437bbdb27f56aab9b6b6d486283ca140f05718
Raphael Wagner <31208448+raalwa@users.noreply.github.com>
2023-11-05 23:21:32 +0100

    Add configuration sample file

---
+++ src/config_EXAMPLE.py
@@ -0,0 +1,14 @@
+LOGS_PATH = 'logs'
+LOG_FILENAME = 'weather_log.csv'
+FIELDNAMES = ['dt_api_call', 'dt', 'visibility', 'clouds', 'uvi', 'temp', 'wind_speed', 'wind_deg',
+              'wind_gust', 'sunrise', 'feels_like', 'sunset', 'pressure', 'dew_point', 'humidity']
+STARTTIME = '00:02:50'
+GOOGLE_DRIVE_FOLDER_ID = ''
+INTERVAL_MEASUREMENTS = 2
+INTERVAL_BACKUP = 2
+# Parameters for API call
+LATITUDE = ''
+LONGITUDE = ''
+API_URL = 'https://api.openweathermap.org/data/3.0/onecall?'
+API_KEY = ''
+EXCLUDE = 'minutely,hourly,daily,alerts'
